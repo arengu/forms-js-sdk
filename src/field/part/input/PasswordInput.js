cfa0c58b30355bbe8b1d26f20253ab4d7a9013c8
jacobovidal <jcvidal90@gmail.com>
2018-10-24 09:43:40 +0200

    Support new hash types for password fields

--- src/field/part/input/PasswordInput.js
+++ src/field/part/input/PasswordInput.js
@@ -1,4 +1,8 @@
 const MD5 = require('crypto-js/md5');
+const SHA1 = require('crypto-js/sha1');
+const SHA256 = require('crypto-js/sha256');
+const SHA512 = require('crypto-js/sha512');
+const SHA3 = require('crypto-js/sha3');
 const BaseInput = require('./BaseInput');
 
 class PasswordInput extends BaseInput {
@@ -48,6 +52,14 @@ class PasswordInput extends BaseInput {
     switch(hash) {
       case 'MD5':
         return MD5(this.node.value).toString();
+      case 'SHA1':
+        return SHA1(this.node.value).toString();
+      case 'SHA256':
+        return SHA256(this.node.value).toString();
+      case 'SHA512':
+        return SHA512(this.node.value).toString();
+      case 'SHA3':
+        return SHA3(this.node.value).toString();
       case 'NONE':
       default:
         return this.node.value;
