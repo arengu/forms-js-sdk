ca809f2772c6bec30d7cda3da7fdaf0936816403
Martín Melado <217925+martinml@users.noreply.github.com>
2020-08-11 11:44:44 +0200

    Oh no

--- src/core/BasePresenters.ts
+++ src/core/BasePresenters.ts
@@ -3,22 +3,22 @@ import { IComponentPresenter, IComponentPresenterListener } from "../component/C
 import { IFormData } from "../form/model/SubmissionModel";
 
 export abstract class BaseComponentPresenter extends ListenableEntity<IComponentPresenterListener> implements IComponentPresenter {
-	public isDynamic(): boolean {
-		return false;
-	}
+  public isDynamic(): boolean {
+    return false;
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public updateContent(this: this, data: IFormData): void {
-		// nothing to do
-	}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public updateContent(this: this, data: IFormData): void {
+    // nothing to do
+  }
 
-	public render(): HTMLElement {
-		throw new Error('Method not implemented.');
-	}
+  public render(): HTMLElement {
+    throw new Error('Method not implemented.');
+  }
 
-	public reset(): void {
-		// nothing to do
-	}
+  public reset(): void {
+    // nothing to do
+  }
 }
 
 export class BaseBlockPresenter extends BaseComponentPresenter { }
