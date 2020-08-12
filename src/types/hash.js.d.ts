9ab514307c14858178b91be3b59b6b41b61d9e2e
Sergio Regueira <sergio.regueira@arengu.com>
2020-08-12 17:13:44 +0200

    Solve tsc errors

---
+++ src/types/hash.js.d.ts
@@ -0,0 +1,14 @@
+declare module 'hash.js/lib/hash/sha/1' {
+  import { sha1 } from 'hash.js';
+  export = sha1;
+}
+
+declare module 'hash.js/lib/hash/sha/256' {
+  import { sha256 } from 'hash.js';
+  export = sha256;
+}
+
+declare module 'hash.js/lib/hash/sha/512' {
+  import { sha512 } from 'hash.js';
+  export = sha512;
+}
